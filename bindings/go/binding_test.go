937bd6d29e054eef5669118bf383b5ed345d6d33
Viruthagiri Thirumavalavan <giridharanbtech@gmail.com>
2026-03-01 03:13:53 +0530

    Fix import path for tree_sitter_vb_dotnet package

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -1,14 +1,14 @@
-package tree_sitter_tree_sitter_vb_dotnet_test
+package tree_sitter_vb_dotnet_test
 
 import (
 	"testing"
 
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-	tree_sitter_tree_sitter_vb_dotnet "github.com/tree-sitter/tree-sitter-tree_sitter_vb_dotnet/bindings/go"
+	tree_sitter_vb_dotnet "github.com/tree-sitter/tree_sitter_vb_dotnet/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
-	language := tree_sitter.NewLanguage(tree_sitter_tree_sitter_vb_dotnet.Language())
+	language := tree_sitter.NewLanguage(tree_sitter_vb_dotnet.Language())
 	if language == nil {
 		t.Errorf("Error loading TreeSitterVbDotnet grammar")
 	}
